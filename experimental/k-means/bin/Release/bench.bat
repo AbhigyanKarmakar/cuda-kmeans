dfada0b00099de5cc5abff387dd519bfa9c617d8
Abhigyan Karmakar <karmakarabhigyan@gmail.com>
2021-06-24 19:18:31 +0530

    Yeet

---
+++ experimental/k-means/bin/Release/bench.bat
@@ -0,0 +1,24 @@
+k-means --N 1M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 2M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 3M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 4M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 8M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 16M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 32M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
+
+k-means --N 1M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 2M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 3M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 4M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 8M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 16M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 32M --dim 20 --k 1k --iterations 20 --algorithm cuda_best
+
+k-means --N 1M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 2M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 3M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 4M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 8M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 16M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+k-means --N 32M --dim 16 --k 1k --iterations 20 --algorithm cuda_best
+
---
+++ experimental/k-means/bin/Release/temp.bat
@@ -0,0 +1,3 @@
+nvprof k-means --N 29M --dim 32 --k 1k --iterations 20 --algorithm cuda_base
+nvprof k-means --N 29M --dim 32 --k 1k --iterations 20 --algorithm cuda_base_shm
+nvprof k-means --N 29M --dim 32 --k 1k --iterations 20 --algorithm cuda_best
